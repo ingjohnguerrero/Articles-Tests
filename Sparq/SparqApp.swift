9d4b5a5c9b197d1f1cf464f35d95a2c278b0fdd5
John Guerrero <ingjohnguerrero@gmail.com>
2025-08-05 12:41:16 +0200

    Initial Commit

---
+++ Sparq/SparqApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparqApp.swift
+//  Sparq
+//
+//  Created by John Guerrero on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SparqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
