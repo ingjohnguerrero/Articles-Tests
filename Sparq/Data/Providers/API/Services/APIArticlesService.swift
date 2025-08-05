e8c1d08b6c3b422dc866e834de1d4cdc5c8d0268
John Guerrero <ingjohnguerrero@gmail.com>
2025-08-05 18:24:26 +0200

    feat: Implement API articles service and data model, enhance ArticlesView with refreshable list

--- Sparq/Data/Providers/API/Services/APIArticlesService.swift
+++ Sparq/Data/Providers/API/Services/APIArticlesService.swift
@@ -7,14 +7,35 @@
 import Foundation
 
 actor APIArticlesService: ArticlesService {
+
+    var cacheStore: SwiftDataArticlesService
+    private let apiURL = URL(string: "https://raw.githubusercontent.com/lanestp/challenge-for-ios/refs/heads/main/test.json")!
+
+    init() {
+        self.cacheStore = try! SwiftDataArticlesService()
+    }
+
     func fetchArticles() async throws -> [Article] {
-        let url = URL(string: "https://raw.githubusercontent.com/lanestp/challenge-for-ios/refs/heads/main/test.json")!
-        let data = try Data(contentsOf: url)
+        let articles = (try? await fetchFromAPI()) ?? []
+        try await saveToStore(articles: articles)
+        return try await cacheStore.fetchArticles()
+    }
+
+    func fetchFromAPI() async throws -> [Article] {
+        let data = try Data(contentsOf: apiURL)
         let decoder = JSONDecoder()
         let articleDTOs = try decoder.decode([ArticleDTO].self, from: data)
         let articles = articleDTOs.map { articleDTO in
             articleDTO.map()
         }
         return articles
+    }
+
+    func saveToStore(articles: [Article]) async throws {
+        guard !articles.isEmpty else { return }
+        try await cacheStore.deleteAllArticles()
+        for article in articles {
+            try await cacheStore.save(article: article)
+        }
     }
 }
